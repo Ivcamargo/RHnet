0c1385b4cd8736417f765dc65a386629f7799085
Ivαn Cesar Camargo <ivan.camargo@comunix.com>
2026-02-10 10:19:55 -0300

    split vite dev module to avoid prod import

--- server/index.ts
+++ server/index.ts
@@ -67,7 +67,7 @@ app.use((req, res, next) => {
   // setting up all the other routes so the catch-all route
   // doesn't interfere with the other routes
   if (isDev) {
-    const { setupVite } = await import("./vite");
+    const { setupVite } = await import("./vite-dev");
     await setupVite(app, server);
   } else {
     serveStatic(app);
