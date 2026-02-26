d4faf3e183092ea89f9d696504a3e2551b63302c
Ivan Camargo <ivancamaracamargo@gmail.com>
2026-02-26 18:27:11 -0300

    Restaura titulo no TopBar da pagina de administrar registros de ponto

--- client/src/pages/admin/time-entries.tsx
+++ client/src/pages/admin/time-entries.tsx
@@ -540,7 +540,7 @@ export default function AdminTimeEntries() {
 
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-slate-900">
-      <TopBar title="Administrar Registros de Ponto" hideTitle />
+      <TopBar title="Administrar Registros de Ponto" />
       <div className="flex">
         <Sidebar />
         <div className="flex-1 p-6">
